4ce075a3f2f66bdcb75f1ba4b6f434cb600cd06f
RahaviSiri <rahavi24siri@gmail.com>
2025-06-30 14:10:46 +0530

    Modify db.js for git safe

--- face-roll-backend/config/db.js
+++ face-roll-backend/config/db.js
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  user: "postgres",
-  password: "Ravi12345",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
