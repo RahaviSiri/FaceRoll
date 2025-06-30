baccdcc84725e921cc1a12786489a92e72b0144e
RahaviSiri <rahavi24siri@gmail.com>
2025-06-30 21:27:28 +0530

    Add UI for add student and home

--- face-roll-frontend/src/App.jsx
+++ face-roll-frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import MarkAttendence from './pages/MarkAttendence'
+import AddStudent from './pages/AddStudent'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mark-attendance" element={<MarkAttendence />} />
+        <Route path="/add-student" element={<AddStudent />} />
+      </Routes>
+    </div>
   )
 }
 
